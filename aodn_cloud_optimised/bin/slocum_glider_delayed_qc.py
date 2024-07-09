568bd74ca8a9c5249e9a8c7d93fa73681b224c27
lbesnard <laurent.besnard@utas.edu.au>
2024-07-09 11:19:53 +1000

    Update configuration

--- aodn_cloud_optimised/bin/slocum_glider_delayed_qc.py
+++ aodn_cloud_optimised/bin/slocum_glider_delayed_qc.py
@@ -6,7 +6,7 @@ def main():
     command = [
         "generic_cloud_optimised_creation",
         "--paths",
-        "IMOS/ANFOG/slocum_glider",
+        "IMOS/ANFOG/slocum_glider/",
         "--dataset-config",
         "slocum_glider_delayed_qc.json",
         "--clear-existing-data",
