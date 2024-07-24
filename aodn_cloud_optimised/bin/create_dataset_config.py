5dfdb32adad0f05ccc3a751d8811765f1f88e810
lbesnard <laurent.besnard@utas.edu.au>
2024-07-24 12:23:08 +1000

    Feat: Improve create_dataset_config to create script and add pyproject entry

--- aodn_cloud_optimised/bin/create_dataset_config.py
+++ aodn_cloud_optimised/bin/create_dataset_config.py
@@ -95,6 +95,111 @@ def generate_template(schema):
     return template
 
 
+def get_module_path():
+    module_name = "aodn_cloud_optimised"
+    spec = importlib.util.find_spec(module_name)
+    module_path = spec.submodule_search_locations[0]
+
+    return module_path
+
+
+def create_dataset_script(dataset_name, dataset_json, nc_file_path, bucket):
+    script_content = f"""#!/usr/bin/env python3
+import subprocess
+
+
+def main():
+    command = [
+        "generic_cloud_optimised_creation",
+        "--paths",
+        "{os.path.dirname(nc_file_path).replace(f's3://{bucket}/','')}",
+        "--dataset-config",
+        "{dataset_json}",
+        "--clear-existing-data",
+        "--cluster-mode",
+        "remote",
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
+    """
+
+    module_path = get_module_path()
+
+    script_path = os.path.join(module_path, "bin", f"{dataset_name}.py")
+    with open(script_path, "w") as script_file:
+        script_file.write(script_content)
+    os.chmod(script_path, 0o755)  # Make the script executable
+    return script_path
+
+
+def update_pyproject_toml(dataset_name):
+    # Locate the pyproject.toml file in the parent directory of the module path
+    module_path = get_module_path()
+    pyproject_path = os.path.abspath(
+        os.path.join(module_path, os.pardir, "pyproject.toml")
+    )
+    script_entry = f'cloud_optimised_{dataset_name} = "aodn_cloud_optimised.bin.{dataset_name}:main"\n'
+
+    with open(pyproject_path, "r") as pyproject_file:
+        lines = pyproject_file.readlines()
+
+    # Initialize variables
+    scripts_section = []
+    in_scripts_section = False
+    section_found = False
+
+    for line in lines:
+        if line.strip() == "[tool.poetry.scripts]":
+            in_scripts_section = True
+            section_found = True
+            scripts_section.append(line)
+            continue
+        if in_scripts_section:
+            if line.strip() == "" or line.startswith("["):
+                in_scripts_section = False
+            else:
+                scripts_section.append(line)
+
+    # Check if the script entry already exists
+    entry_exists = any(script_entry.strip() in line for line in scripts_section)
+    if entry_exists:
+        return
+
+    # Add the new script entry
+    scripts_section.append(script_entry)
+
+    # Sort the script entries alphabetically
+    sorted_scripts = [scripts_section[0]] + sorted(
+        scripts_section[1:], key=lambda x: x.split("=")[0].strip()
+    )
+
+    # Write the updated pyproject.toml
+    with open(pyproject_path, "w") as pyproject_file:
+        in_scripts_section = False
+        for line in lines:
+            if line.strip() == "[tool.poetry.scripts]":
+                in_scripts_section = True
+                pyproject_file.write(line)
+                for script in sorted_scripts[1:]:
+                    pyproject_file.write(script)
+            elif in_scripts_section and (line.strip() == "" or line.startswith("[")):
+                in_scripts_section = False
+                pyproject_file.write(line)
+            elif not in_scripts_section:
+                pyproject_file.write(line)
+
+        # If the [tool.poetry.scripts] section was not found, add it at the end
+        if not section_found:
+            pyproject_file.write("\n[tool.poetry.scripts]\n")
+            for script in sorted_scripts[1:]:
+                pyproject_file.write(script)
+
+
 def main():
     """
     Script to generate a dataset configuration from a NetCDF file stored in S3.
@@ -106,7 +211,8 @@ def main():
     3. Populates the dataset configuration with additional metadata including dataset name, metadata UUID, logger name,
        cloud-optimised format, cluster options, and batch size.
     4. Writes the dataset configuration to the module path as a JSON file. READY TO BE ADDED TO GITHUB
-    5. Optionally, fills up the AWS registry with Geonetwork metadata if a UUID is provided.
+    5. A script is also created under the bin folder of the module (to be updated manually) and the entry is added to the pyproject.toml file
+    6. Optionally, fills up the AWS registry with Geonetwork metadata if a UUID is provided.
 
     Usage:
         cloud_optimised_create_dataset_config -f <NetCDF file object key> -c <cloud optimised format> -d <dataset name> [-b <S3 bucket name>] [-u <Geonetwork Metadata UUID>]
@@ -227,6 +333,11 @@ def main():
     # write json config to module path
     with open(f"{module_path}/config/dataset/{args.dataset_name}.json", "w") as f:
         json.dump(dataset_config, f, indent=2)
+
+    create_dataset_script(
+        args.dataset_name, f"{args.dataset_name}.json", nc_file, bucket
+    )
+    update_pyproject_toml(args.dataset_name)
 
     # fill up aws registry with GN3 uuid
     if args.uuid:
