8984a304e6f9e86e11242d4bcfd1b6896a778070
lbesnard <laurent.besnard@utas.edu.au>
2024-10-02 14:39:49 +1000

    Feat: change default value of spatial extent

--- aodn_cloud_optimised/bin/create_dataset_config.py
+++ aodn_cloud_optimised/bin/create_dataset_config.py
@@ -681,6 +681,7 @@ def main():
         "worker_options": {"nthreads": 4, "memory_limit": "8GB"},
     }
     dataset_config["batch_size"] = 5
+    dataset_config["spatial_extent"]["spatial_resolution"] = 5
 
     if args.cloud_format == "parquet":
         # default partition keys
