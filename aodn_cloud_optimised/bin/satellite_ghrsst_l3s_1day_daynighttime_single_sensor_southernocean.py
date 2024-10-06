cbfa9a95b766e68e70b598fcfb9ac00bc6c71c41
lbesnard <laurent.besnard@utas.edu.au>
2024-10-07 10:30:26 +1100

    Feat: add new SRS SST Southern collections

---
+++ aodn_cloud_optimised/bin/satellite_ghrsst_l3s_1day_daynighttime_single_sensor_southernocean.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+def main():
+    command = [
+        "generic_cloud_optimised_creation",
+        "--paths",
+        "IMOS/SRS/SST/ghrsst/L3S-1dS/dn/2024",
+        "IMOS/SRS/SST/ghrsst/L3S-1dS/dn/2023",
+        # "--filters",
+        # "FILTER_STRING_1",
+        # "FILTER_STRING_1",
+        "--dataset-config",
+        "satellite_ghrsst_l3s_1day_daynighttime_single_sensor_southernocean.json",
+        "--clear-existing-data",
+        "--cluster-mode",
+        "remote",
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
