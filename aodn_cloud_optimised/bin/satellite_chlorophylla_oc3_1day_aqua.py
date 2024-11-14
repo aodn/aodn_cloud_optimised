21c42a088243475f7d29cfc8904f2bb1f77477e7
lbesnard <laurent.besnard@utas.edu.au>
2024-11-14 16:15:32 +1100

    Feat: add new SRS OC collections

---
+++ aodn_cloud_optimised/bin/satellite_chlorophylla_oc3_1day_aqua.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+def main():
+    command = [
+        "generic_cloud_optimised_creation",
+        "--paths",
+        "IMOS/SRS/OC/gridded/aqua/P1D/",
+        "--filters",
+        "aust.chl_oc3.nc",
+        "--dataset-config",
+        "satellite_chlorophylla_oc3_1day_aqua.json",
+        "--clear-existing-data",
+        "--cluster-mode",
+        "local",
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ aodn_cloud_optimised/bin/satellite_chlorophylla_oci_1day_aqua.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+## issue opening files in batch with h5netcdf. The ds does not have any variables, To investigate
+
+
+def main():
+    command = [
+        "generic_cloud_optimised_creation",
+        "--paths",
+        "IMOS/SRS/OC/gridded/aqua/P1D/2024",
+        "--filters",
+        "aust.chl_oc3.nc",
+        "--dataset-config",
+        "satellite_chlorophylla_oci_1day_aqua.json",
+        "--clear-existing-data",
+        "--cluster-mode",
+        "local",
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
