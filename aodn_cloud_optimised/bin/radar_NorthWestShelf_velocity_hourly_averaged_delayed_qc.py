e21a2220d0f4a61cc3340e58031f6c60253e40ac
lbesnard <laurent.besnard@utas.edu.au>
2024-11-18 17:00:06 +1100

    Fix: update radar config with appropriate chunk size

---
+++ aodn_cloud_optimised/bin/radar_NorthWestShelf_velocity_hourly_averaged_delayed_qc.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import subprocess
+
+
+def main():
+    command = [
+        "generic_cloud_optimised_creation",
+        "--paths",
+        "IMOS/ACORN/gridded_1h-avg-current-map_QC/NWA/",
+        # "--filters",
+        # "FILTER_STRING_1",
+        # "FILTER_STRING_1",
+        "--dataset-config",
+        "radar_NorthWestShelf_velocity_hourly_averaged_delayed_qc.json",
+        "--clear-existing-data",
+        "--cluster-mode",
+        "remote",
+    ]
+
+    # Run the command
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+    main()
